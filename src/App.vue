40274ef6d675b413254d4d8e391daa2cb90fe4f8
Marilyn  Negrete <marilynnegrete@Marilyns-MacBook-Air.local>
2018-07-24 12:08:41 -0500

    created ProductList component to display the products

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <ProductList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ProductList from './components/ProductList'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   ProductList
   }
 }
 </script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="list">
+<ul>
+  <li v-for="product in products">{{ product.title }} - {{ product.price }}</li>
+</ul>
+</div>
+
+</template>
+
+<script>
+import shop from '@/api/shop'
+
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  created () {
+    shop.getProducts(products => {
+      this.products = products
+    })
+  }
+
+}
+</script>
+
+<style>
+
+</style>
